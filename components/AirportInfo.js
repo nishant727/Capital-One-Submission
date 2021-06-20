413ef5f993e28028903d950d47a6e0b58f6e5102
nishant727 <86209171+nishant727@users.noreply.github.com>
2021-06-20 19:09:50 -0400

    Update AirportInfo.js

--- components/AirportInfo.js
+++ components/AirportInfo.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './AirportInfo.css';
 
+//Following format of workshop
 function AirportInfo() { 
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e);
+        async function fetchOMDB() {
+            const searchParam = encodeURIComponent(query);
+            const apiURL = 'http://www.omdbapi.com/?i=tt3896198&apikey=6952c71f';
+            console.log(apiURL);
+            let reponse = await fetch(apiURL);
+            response = await response.json();
+            console.log(response.Search);
+            setAirports  = (response.Search);
+        }
+        fetchOMDB();
+        setQuery('');
+    }
     return;
 }
 
